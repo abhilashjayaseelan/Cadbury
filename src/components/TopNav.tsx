bb128ad39b3533e7cdcaa9d639d37efe47a8ae88
Abhilash MJ <abhialash.jayaseelan@gmail.com>
2023-10-10 08:31:02 +0530

    nav icons added

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,19 +1,31 @@
-import { Flex, Tab, TabList, Tabs } from "@chakra-ui/react";
+import { Box, Flex, Tab, TabList, Tabs, Text } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 
+import {SiMarketo} from "react-icons/si";
+import {BiMobile} from "react-icons/bi";
+import {MdOutlineGroupWork, MdWorkOutline} from "react-icons/md";
+import {RiShapesLine} from "react-icons/ri";
+import {FaListUl} from "react-icons/fa6";
+import {AiOutlineBell} from "react-icons/ai";
+import {BsCollectionPlay, BsCurrencyPound, BsFillBarChartLineFill} from "react-icons/bs";
+import {MdOutlineReportProblem, MdOutlineAdminPanelSettings} from "react-icons/md";
+
+
+
+
 const tabs = [
-  { label: "Dashboard", to: "/" },
-  { label: "Markets", to: "/markets" },
-  { label: "Machines", to: "/machines" },
-  { label: "Operations", to: "/operations" },
-  { label: "Products", to: "/products" },
-  { label: "Transactions", to: "/transactions" },
-  { label: "Events", to: "/events" },
-  { label: "Content", to: "/content" },
-  { label: "Jobs", to: "/jobs" },
-  { label: "Billing", to: "/billing" },
-  { label: "Reports", to: "/reports" },
-  { label: "Admin", to: "/admin" },
+  { label: "Dashboard", to: "/", icon: <BsFillBarChartLineFill/>},
+  { label: "Markets", to: "/markets", icon: <SiMarketo/> },
+  { label: "Machines", to: "/machines", icon: <BiMobile/> },
+  { label: "Operations", to: "/operations", icon: <MdOutlineGroupWork/> },
+  { label: "Products", to: "/products", icon: <RiShapesLine/> },
+  { label: "Transactions", to: "/transactions", icon: <FaListUl/> },
+  { label: "Events", to: "/events", icon: <AiOutlineBell/> },
+  { label: "Content", to: "/content", icon: <BsCollectionPlay/> },
+  { label: "Jobs", to: "/jobs", icon: <MdWorkOutline/> },
+  { label: "Billing", to: "/billing", icon: <BsCurrencyPound/> },
+  { label: "Reports", to: "/reports", icon: <MdOutlineReportProblem/> },
+  { label: "Admin", to: "/admin", icon: <MdOutlineAdminPanelSettings/> },
 ];
 
 const TopNav = () => {
@@ -27,7 +39,12 @@ const TopNav = () => {
         <TabList>
           {tabs.map((tab, index) => (
             <Tab w={{ md: "120px" }} key={index}>
-              <NavLink to={tab?.to}>{tab?.label}</NavLink>
+              <NavLink to={tab?.to}>
+                <Box display="flex" alignItems="center">
+                  <Box mr="2" color="blue">{tab?.icon}</Box>
+                  <Text fontSize='sm'>{tab?.label}</Text>  
+                </Box>
+              </NavLink>
             </Tab>
           ))}
         </TabList>
